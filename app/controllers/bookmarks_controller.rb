9c922e8c2d7c25c05310f831c5eaf83e96c06db3
Lucas Moreno <lucaslopesmoreno@gmail.com>
2024-02-27 11:10:39 -0300

    finished back-end

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @list = List.find(params[:list_id])
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: "Movie successefully added to list."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,30 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: "List successefully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
